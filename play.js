d68328c8356771d2ba2bc5872bf43b5f1e92b923
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-11-29 11:16:48 -0500

    Moved styles to html

--- play.js
+++ play.js
@@ -4,20 +4,7 @@ window.onload = (e) => {
 	wrapper.id = 'wrapper';
 	document.body.appendChild(wrapper);
 
-	wrapper.setAttribute('style', `
-		width: 900px;
-		margin: 0 auto;
-		border: 1px solid #888;
-		padding: 20px;
-	`);
-
 	let fightForm = document.createElement('form');
-	fightForm.setAttribute('style', `
-		padding: 20px;
-		border: 1px solid #888;
-		width: 400px;
-		margin: 20px auto;
-	`);
 	let player1Sel = document.createElement('select');
 	let label = document.createElement('label');
 	label.innerText = 'vs';
